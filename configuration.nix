6da90908f7fce1a31a13d628f5ff7d038acfc401
rene <renedes@protonmail.com>
2023-10-26 21:21:51 -0400

    Clean up configuration

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 {
@@ -13,25 +9,18 @@
     ];
 
   # Use the systemd-boot EFI boot loader.
+  # TODO maybe use grub?
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "crab"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # === NETWORKING ===
+  networking.hostName = "crab";
   networking.networkmanager.enable = true;
 
   nix.settings.experimental-features = "nix-command flakes";
 
-  # Set your time zone.
+  # === LOCALE, etc === 
   time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -46,7 +35,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
   services.spice-vdagentd.enable = true;
@@ -60,32 +48,27 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-
-  users.mutableUsers = false;
-  users.users.rene = {
-    isNormalUser = true;
-    description = "rene";
-    extraGroups = [ "networkmanager" "wheel" "dialout" ];
-    hashedPasswordFile = "/nix/persist/keystore/passwords/rene";
-    packages = with pkgs; [
-      
-    ];
+  # === USERS ===
+  users = {
+    mutableUsers = false;
+    users = {
+      rene = {
+        isNormalUser = true;
+        description = "rene";
+        extraGroups = [ "networkmanager" "wheel" "dialout" ];
+        hashedPasswordFile = "/nix/persist/keystore/passwords/rene"; # See utils/mkpasswd.sh
+        shell = pkgs.fish;
+      };
+      root.hashedPasswordFile = "/nix/persist/keystore/passwords/root";
+    };
   };
-  users.users.root.hashedPasswordFile = "/nix/persist/keystore/passwords/root";
 
-  users.users.rene.shell = pkgs.fish;
   programs.fish.enable = true;
   environment.pathsToLink = ["/share/fish"];
 
-  # List packages installed in system profile. To search, run:
+  # === SYSTEM PACKAGES ===
   environment.systemPackages = with pkgs; [
     vim
     spice-vdagent
@@ -93,28 +76,5 @@
     home-manager
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-
 }
